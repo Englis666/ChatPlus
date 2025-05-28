9fbfe08792085d96562fd2e1a36f7f341886b410
Englis666 <eabarros2610@gmail.com>
2025-05-27 22:11:02 -0500

    feature: Se realiza el registro frontend para empezar compo platntilla

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('title', 'Registro')
+
+@section('content')
+<div class="d-flex justify-content-center align-items-center vh-100 bg-light">
+    <div class="card shadow-sm p-4" style="width: 100%; max-width: 400px; border-radius: 12px;">
+        <h3 class="mb-4 text-center fw-bold text-primary">Registro</h3>
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+
+            <div class="mb-3">
+                <label for="email" class="form-label fw-semibold">Nombre Completo</label>
+                <input type="email" id="email" name="email" class="form-control form-control-lg" placeholder="tucorreo@ejemplo.com" required autofocus>
+            </div>
+
+            <div class="mb-3">
+                <label for="email" class="form-label fw-semibold">Nombre De Usuario</label>
+                <input type="email" id="email" name="email" class="form-control form-control-lg" placeholder="tucorreo@ejemplo.com" required autofocus>
+            </div>
+
+            <div class="mb-3">
+                <label for="email" class="form-label fw-semibold">Correo Electronico<</label>
+                <input type="email" id="email" name="email" class="form-control form-control-lg" placeholder="tucorreo@ejemplo.com" required autofocus>
+            </div>
+
+            <div class="mb-4">
+                <label for="password" class="form-label fw-semibold">Contraseña</label>
+                <input type="password" id="password" name="password" class="form-control form-control-lg" placeholder="********" required>
+            </div>
+
+            
+
+            <button type="submit" class="btn btn-primary btn-lg w-100 rounded-pill shadow-sm">
+                Registrarse
+            </button>
+        </form>
+
+        <div class="mt-3 text-center">
+            <small class="text-muted">¿Ya tienes cuenta? <a href="{{ route('login') }}">Regístrate</a></small>
+        </div>
+    </div>
+</div>
+@endsection
