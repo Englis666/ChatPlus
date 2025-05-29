6f54262c14967c6d39e9abbda15e6f8a6e8516c6
Englis666 <eabarros2610@gmail.com>
2025-05-28 21:18:51 -0500

    feat: componente summary de el dashboard que recuenta las tareas pendientes documentos unicos y mensajes

---
+++ laravel-app/resources/views/components/cards/summary.blade.php
@@ -0,0 +1,21 @@
+<div class="row g-4 mb-5">
+                <div class="col-md-4">
+                    <div class="summary-card">
+                        <i class="ri-todo-line card-icon"></i>
+                        <h6 class="text-muted">Tareas Pendientes</h6>
+                        <h2 class="text-dark animate-number" data-target="1340">0</h2>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="summary-card">
+                        <i class="ri-file-text-line card-icon"></i>
+                        <h6 class="text-muted">Documentos Únicos</h6> <h2 class="text-dark animate-number" data-target="1">0</h2>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="summary-card">
+                        <i class="ri-mail-line card-icon"></i>
+                        <h6 class="text-muted">Mensajes Nuevos</h6> <h2 class="text-dark animate-number" data-target="76">0</h2>
+                    </div>
+                </div>
+            </div>
