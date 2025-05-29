f1ee6d26318559f9435167a4897fa14adb75b750
Englis666 <eabarros2610@gmail.com>
2025-05-28 21:19:07 -0500

    Feat: Layout de el registro aqui va toda la segmentacion HTML5

---
+++ laravel-app/resources/views/layouts/authForms/register.blade.php
@@ -0,0 +1,21 @@
+{{-- resources/views/layouts/authForm.blade.php --}}
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Register Form - DevSync')</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link href="{{ asset('css/forms/RegisterForm.css') }}" rel="stylesheet">
+    
+</head>
+<body>
+
+    <main>
+        @yield('content') 
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
