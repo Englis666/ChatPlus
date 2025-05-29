5ca418ff0fd9c071b2786da28c50a893b7dabd33
Englis666 <eabarros2610@gmail.com>
2025-05-28 21:19:38 -0500

    Feat: sidebar modularizado

---
+++ laravel-app/resources/views/components/sidebar.blade.php
@@ -0,0 +1,20 @@
+<nav class="col-md-2 d-none d-md-block sidebar">
+    <div class="sidebar-header">
+        <i class="ri-compasses-2-line"></i>
+        <h5 class="app-title">DevSync</h5>
+    </div>
+    <ul class="nav flex-column">
+        <li class="nav-item"><a class="nav-link active" href="#"><i class="ri-dashboard-line ri-icon"></i> Dashboard</a></li>
+        <li class="nav-item"><a class="nav-link" href="#"><i class="ri-folder-open-line ri-icon"></i> Proyectos</a></li>
+        <li class="nav-item"><a class="nav-link" href="#"><i class="ri-message-3-line ri-icon"></i> Mensajes</a></li>
+        <li class="nav-item"><a class="nav-link" href="#"><i class="ri-settings-4-line ri-icon"></i> Configuración</a></li>
+    </ul>
+
+    <div class="user-profile-sidebar">
+        <img src="https://i.pravatar.cc/150?img=68" alt="Renee McKelvey Avatar">
+        <div>
+            <div class="user-name">Renee McKelvey</div>
+            <div class="user-role">Product Manager</div>
+        </div>
+    </div>
+</nav>
