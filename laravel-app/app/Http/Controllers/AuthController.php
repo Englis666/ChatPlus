c05808b9682adc15d1b223bef35f38c1cafeb7ca
Englis666 <eabarros2610@gmail.com>
2025-06-06 21:55:21 -0500

    update: se realiza los nuevos metodos de login y register para el controlador <AuthController> siguiendo las practicas DDD profesionaels

--- laravel-app/app/Http/Controllers/AuthController.php
+++ laravel-app/app/Http/Controllers/AuthController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\User;
+use App\Infrastructure\Persistence\User\EloquentUserRepsository;
+use App\Application\User\UseCase\LoginService;
+use App\Application\User\UseCase\RegisterService;
+
 
 class AuthController extends Controller{
 
@@ -11,9 +14,37 @@ class AuthController extends Controller{
         return view('auth.login');
     }
 
+    public function login(Request $request){
+        $credentials = $request->only('email','password');
+        $userRepo = new EloquentUserRepsository();
+        $loginService = new LoginService($userRepo);
+
+        if ($loginService->login($credentials['email'], $credentials['password'])) {
+           $user = $userRepo->findByEmail($credentials['email']);
+            Auth::LoginUsingId($user->$idUser);
+            return redirect()->route('dashboard');
+        }
+        return back()->withErrors(['login' => 'Credenciales incorrectas.']);
+    }
+
     public function showRegister(){
         return view('auth.register');
     }
+
+    public function register(Request $request){
+        $data = $request->only('name', 'email', 'password');
+        $userRepo = new EloquentUserRepsository();
+        $registerService = new RegisterService($userRepo);
+        $user = $registerService->register($data['name'], $data['email'], $data['password']);
+        if ($user) {
+            Auth::LoginUsingId($user->idUser);
+            return redirect()->route('dashboard');
+        }
+        return back()->withErrors(['register' => 'Error al registrar el usuario.']); 
+    }
+
+
+
 
 
 }
