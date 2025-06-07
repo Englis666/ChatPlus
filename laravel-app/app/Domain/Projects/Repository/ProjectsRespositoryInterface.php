619565b8a0fc3ed4df24c3209fc9112fa62b076d
Englis666 <eabarros2610@gmail.com>
2025-06-06 22:19:01 -0500

    feat: add ProjectsRepositoryInterface for project data retrieval

---
+++ laravel-app/app/Domain/Projects/Repository/ProjectsRespositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Domain\Projects\Repository;
+
+use App\Domain\Projects\Entities\Projects;
+
+class ProjectsRepositoryInterface
+{
+    /**
+     * Get all Projects of the 
+     */
+
+
+
+}
